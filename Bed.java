a862a637f4c1db1b3e58e9d57574c5cc0da2d286
S4MWi83 <50416286+S4MWi83@users.noreply.github.com>
2020-05-14 10:56:02 +0200

    Add files via upload

--- Bed.java
+++ Bed.java
@@ -1,10 +1,12 @@
+import java.io.File;
 
 public class Bed implements Furniture{
 
 	@Override
-	public void show() {
-		//Visa bilden av sängen på JFrame
+	public File getPicture() {
+		
+		return picture;
 		
 	}
-
+	private File picture = new File("\"C:/Users/samue/OneDrive/Bilder/Prog/chair.jpg");
 }
