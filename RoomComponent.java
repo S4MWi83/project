4c6658de64665583dda436c87db18281765b3956
rebeckaberg97 <65337546+rebeckaberg97@users.noreply.github.com>
2020-05-14 15:59:38 +0200

    Update RoomComponent.java

--- RoomComponent.java
+++ RoomComponent.java
@@ -10,27 +10,32 @@ public class RoomComponent extends JPanel  {
 		super.paintComponent(g);
 	
 		g.setColor(Color.black);
+		
 		//Left outline
-		g.drawLine(0, 0, 0, 481);
-		g.drawLine(1, 1, 1, 481);
-		g.drawLine(2, 2, 2, 481);
+		g.drawLine(0, 0, 0, 480);
+		g.drawLine(1, 1, 1, 480);
+		g.drawLine(2, 2, 2, 480);
+		
 		//Top outline
-		g.drawLine(0, 0, 781, 0);
-		g.drawLine(1, 1, 781, 1);
-		g.drawLine(2, 2, 781, 2);
+		g.drawLine(0, 0, 780, 0);
+		g.drawLine(1, 1, 780, 1);
+		g.drawLine(2, 2, 780, 2);
+		
 		//Right outline
 		g.drawLine(778, 0, 778, 478);
 		g.drawLine(779, 0, 779, 479);
 		g.drawLine(780, 0, 780, 480);
+		
 		//Bottom outline
 		g.drawLine(0, 478, 778, 478);
 		g.drawLine(1, 479, 779, 479);
 		g.drawLine(2, 480, 780, 480);
+		
 		//Walls
-		g.drawLine(363, 0, 363, 123);
+		g.drawLine(363, 3, 363, 123);
 		g.drawLine(363, 183, 363, 303);
-		g.drawLine(363, 363, 363, 481);
-		g.drawLine(363, 243, 781, 243);
+		g.drawLine(363, 363, 363, 479);
+		g.drawLine(363, 243, 779, 243);
 
 		
 		
