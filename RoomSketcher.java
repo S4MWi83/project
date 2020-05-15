cc3f466c28ce50e5d01606ed70f92c1ee5272de5
S4MWi83 <50416286+S4MWi83@users.noreply.github.com>
2020-05-15 13:45:44 +0200

    Update RoomSketcher.java

--- RoomSketcher.java
+++ RoomSketcher.java
@@ -6,9 +6,7 @@ import javax.swing.JPanel;
 
 public class RoomSketcher extends Designer {
 	
-	public RoomSketcher() {
-		
-	}
+	public RoomSketcher() {}
 	
 	@Override	
 	public JPanel createBackComponent() {
@@ -19,14 +17,14 @@ public class RoomSketcher extends Designer {
 	
 	@Override
 	public JLayeredPane createPicturePane() {
-		//drag and drop component
+		//drag and drop images
 		picpane = new PicturePane();
-		return null;
+		return picpane;
 	}
 	
 	@Override
 	public void saveButtonPressed() {
-		
+		designs[count] = new DesignProject("project", picpane);
 	}
 	
 	@Override
@@ -34,20 +32,61 @@ public class RoomSketcher extends Designer {
 		
 	}
 	
+	
 	@Override
 	public void bedButtonPressed() {
-		Furniture bed = shoppingList.getFurniture("bed");
-		pics[count] = bed.getPicture();
-		comp.addFurniture(pics[count]);
-		count++;
+		Furniture f = shoppingList.getFurniture("bed");
+		pic = f.getPicture();
+		picpane.addFurniture(pic);
 	}
-
+	
+	@Override
+	public void sofaButtonPressed() {
+		Furniture f = shoppingList.getFurniture("sofa");
+		pic = f.getPicture();
+		picpane.addFurniture(pic);
+	}
+	
+	@Override
+	public void diningButtonPressed() {
+		Furniture f = shoppingList.getFurniture("diningtable");
+		pic = f.getPicture();
+		picpane.addFurniture(pic);
+	}
+	
+	@Override
+	public void deskButtonPressed() {
+		Furniture f = shoppingList.getFurniture("desk");
+		pic = f.getPicture();
+		picpane.addFurniture(pic);
+	}
+	
+	@Override
+	public void bedsideTableButtonPressed() {
+		Furniture f = shoppingList.getFurniture("bedsidetable");
+		pic = f.getPicture();
+		picpane.addFurniture(pic);
+	}
+	
+	@Override
+	public void chairButtonPressed() {
+		Furniture f = shoppingList.getFurniture("chair");
+		pic = f.getPicture();
+		picpane.addFurniture(pic);
+	}
+	
+	@Override
+	public void wardrobeButtonPressed() {
+		Furniture f = shoppingList.getFurniture("wardrobe");
+		pic = f.getPicture();
+		picpane.addFurniture(pic);
+	}
 
 	private FurnitureFactory shoppingList = new FurnitureFactory();
 	private RoomComponent comp;
 	private PicturePane picpane;
 	private DesignProject[] designs = new DesignProject[25];
-	private File[] pics = new File[25];
+	private File pic = new File("");
 	private int count = 0;
 	
 }
